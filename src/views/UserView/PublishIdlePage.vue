59887c304e02c34ecec06ad636cea571c8de4453
Ash-2000 <60924645+Ash-2000@users.noreply.github.com>
2023-10-05 09:27:49 +1100

    add more pages
    add seek trade page and public idle item page

---
+++ src/views/UserView/PublishIdlePage.vue
@@ -0,0 +1,90 @@
+<template>
+
+  <div class="commentField" style="padding-bottom: 5rem; display: flex; flex-direction: column">
+    <div class="division">
+      <div class="item-basic-info">
+        <div class="item-basic-info-element">
+          <h3>basic information</h3>
+          <input class="info-input" type="text">
+        </div>
+
+        <div class="item-basic-info-element">
+          <h3>basic information</h3>
+          <input class="info-input" type="text">
+        </div>
+
+        <div class="item-basic-info-element">
+          <h3>basic information</h3>
+          <input class="info-input" type="text">
+        </div>
+      </div>
+    </div>
+
+
+    <div class="division">
+      <h3>item description</h3>
+      <input style="width: 80%; align-items: center; height: 100px; margin-bottom: 50px; border-radius: 10px; border: gray solid">
+    </div>
+
+    <div class="division">
+      <h3>item image</h3>
+    </div>
+
+    <div class="division">
+      <h3>item image</h3>
+    </div>
+
+
+
+
+
+  </div>
+</template>
+<script>
+export default {
+  name: "PublishIdlePage"
+}
+</script>
+
+<style scoped>
+.item-basic-info
+{
+  display: flex;
+  flex-direction: row;
+
+}
+.commentField {
+  align-items: left;
+  width: 80%;
+  height: 180px;
+  margin-bottom: 5px;
+  margin-top: 5px;
+}
+.item-basic-info-element {
+  display: flex;
+  flex-direction: column;
+  border: gray 2px solid;
+  border-radius: 10px;
+  margin-left: 10%;
+  width: 200px;
+}
+.info-input{
+  border-radius:5px;
+  height: 20px;
+  border: 1px gray solid;
+  margin-bottom: 15px;
+  margin-left: 15px;
+  margin-right: 15px;
+
+}
+.division_basic_info
+{
+
+
+}
+
+.division{
+  /*border: gray solid;*/
+  margin-top: 15px;
+}
+</style>
---
+++ src/views/UserView/SeekTradeItem.vue
@@ -0,0 +1,25 @@
+<template>
+  <div style="padding-bottom: 5rem; display: flex">
+    <div class="item-basic-info">
+      <div>
+        basic information
+      </div>
+    </div>
+
+
+
+  </div>>
+</template>
+
+<script>
+export default {
+  name: "SeekTradeItem",
+  data(){
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
